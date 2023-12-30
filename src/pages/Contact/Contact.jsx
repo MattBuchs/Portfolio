2cc62bc4a6700e05dcc26f6ce5ee237ca8e9aabc
MattBuchs <mattbuchs25@gmail.com>
2023-12-30 16:01:41 +0100

    verification for sending the email

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 export default function Contact() {
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [emailError, setEmailError] = useState();
+    const [messageError, setMessageError] = useState();
+
     function sendEmail(e) {
         e.preventDefault();
+
+        let name = e.target[0].value;
+        let company = e.target[1].value;
+
+        if (name === "") name = `<em>Donnée non rentrée</em>`;
+        if (company === "") company = `<em>Donnée non rentrée</em>`;
+        if (email === "") setEmailError(true);
+        if (message === "") setMessageError(true);
+        if (message === "" || email === "") return;
 
         // eslint-disable-next-line no-undef
         Email.send({
@@ -9,14 +25,14 @@ export default function Contact() {
             Password: import.meta.env.VITE_PASSWORD,
             To: import.meta.env.VITE_EMAIL,
             From: import.meta.env.VITE_EMAIL,
-            Subject: "This is the subject",
-            Body: "And this is the body",
+            Subject: "Portfolio Message",
+            Body: `<strong>Nom/Prénom :</strong> ${name}<br><strong>Entreprise :</strong> ${company}<br><strong>Email :</strong> ${email}<br><strong>Message :</strong> ${message}`,
         }).then((message) => alert(message));
     }
 
     return (
-        <div className="h-full text-slate-100 flex justify-center">
-            <div className="bg-[#120b2a] px-14 py-10 my-20 rounded-md border border-slate-100/30 shadow-md shadow-slate-100/15">
+        <div className="h-full text-slate-100 flex justify-center items-center">
+            <div className="bg-[#120b2a] p-4 sm:px-14 sm:py-10 my-10 sm:my-20 rounded-md border border-slate-100/30 shadow-md shadow-slate-100/15 overflow-auto">
                 <h2 className="text-4xl underline underline-offset-8 decoration-yellow-300">
                     Contact
                 </h2>
@@ -27,36 +43,70 @@ export default function Contact() {
                         mes compétences, votre entreprise, vos projets...
                     </p>
                 </section>
-                <form onSubmit={sendEmail} className="mt-14">
-                    <section className="flex">
+                <form onSubmit={sendEmail} className="mt-10">
+                    <section className="flex flex-col sm:flex-row">
                         <div>
                             <label htmlFor="name" className="block">
                                 Nom & Prénom
                             </label>
-                            <input type="text" id="name" />
+                            <input
+                                type="text"
+                                id="name"
+                                className="text-slate-950 w-full sm:w-[189px] sm:mr-6"
+                            />
                         </div>
                         <div>
                             <label htmlFor="company" className="block">
                                 Entreprise
                             </label>
-                            <input type="text" id="company" />
+                            <input
+                                type="text"
+                                id="company"
+                                className="text-slate-950 w-full sm:w-[189px]"
+                            />
                         </div>
                     </section>
                     <div>
-                        <label htmlFor="email" className="block">
-                            Email
-                        </label>
-                        <span> *</span>
-                        <input type="text" id="email" />
+                        <div>
+                            <label htmlFor="email">Email</label>
+                            <span className="text-yellow-300 text-xs"> *</span>
+                        </div>
+                        <input
+                            type="email"
+                            id="email"
+                            className="text-slate-950 w-full sm:w-[402px]"
+                            onChange={(e) => {
+                                setEmail(e.target.value);
+                                setEmailError(false);
+                            }}
+                        />
+                        {emailError && (
+                            <p className="text-red-600 text-sm">
+                                Veuillez remplir ce champ
+                            </p>
+                        )}
                     </div>
                     <div>
-                        <label htmlFor="message" className="block">
-                            Votre message
-                        </label>
-                        <span> *</span>
-                        <textarea type="text" id="message" />
+                        <div>
+                            <label htmlFor="message">Votre message</label>
+                            <span className="text-yellow-300 text-xs"> *</span>
+                        </div>
+                        <textarea
+                            type="text"
+                            id="message"
+                            className="text-slate-950 w-full sm:w-[402px]"
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                                setMessageError(false);
+                            }}
+                        />
+                        {messageError && (
+                            <p className="text-red-600 text-sm">
+                                Veuillez remplir ce champ
+                            </p>
+                        )}
                     </div>
-                    <p>
+                    <p className="text-yellow-200 text-xs">
                         * Ces champs sont réquis pour pouvoir envoyer votre
                         message
                     </p>
