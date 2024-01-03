d34abfac96f6349b20d669c29da9b97c23c6a98d
MattBuchs <mattbuchs25@gmail.com>
2024-01-03 19:08:12 +0100

    animation on the cards of the work page

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -1,8 +1,11 @@
+import "./border-animation.css";
+
 export default function Work() {
     const work = [
         {
             id: 1,
             img: "/avatar.svg",
+            title: "Titre",
             text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur eos deserunt molestiae rerum porro, delectus illo iste, numquam laborum vel quis ipsa cum, facilis voluptas. Eligendi nisi officiis labore tempora!",
             demoLink: "#",
             sourceCodeLink: "#",
@@ -10,6 +13,7 @@ export default function Work() {
         {
             id: 2,
             img: "/avatar.svg",
+            title: "Titre",
             text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur eos deserunt molestiae rerum porro, delectus illo iste, numquam laborum vel quis ipsa cum, facilis voluptas. Eligendi nisi officiis labore tempora!",
             demoLink: "#",
             sourceCodeLink: "#",
@@ -17,6 +21,7 @@ export default function Work() {
         {
             id: 3,
             img: "/avatar.svg",
+            title: "Titre",
             text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur eos deserunt molestiae rerum porro, delectus illo iste, numquam laborum vel quis ipsa cum, facilis voluptas. Eligendi nisi officiis labore tempora!",
             demoLink: "#",
             sourceCodeLink: "#",
@@ -24,6 +29,7 @@ export default function Work() {
         {
             id: 4,
             img: "/avatar.svg",
+            title: "Titre",
             text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur eos deserunt molestiae rerum porro, delectus illo iste, numquam laborum vel quis ipsa cum, facilis voluptas. Eligendi nisi officiis labore tempora!",
             demoLink: "#",
             sourceCodeLink: "#",
@@ -31,6 +37,7 @@ export default function Work() {
         {
             id: 5,
             img: "/avatar.svg",
+            title: "Titre",
             text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur eos deserunt molestiae rerum porro, delectus illo iste, numquam laborum vel quis ipsa cum, facilis voluptas. Eligendi nisi officiis labore tempora!",
             demoLink: "#",
             sourceCodeLink: "#",
@@ -38,28 +45,40 @@ export default function Work() {
     ];
 
     return (
-        <div className="flex flex-col">
-            {work.map((obj) => (
-                <section
-                    key={obj.id}
-                    className={`bg-red-500 w-2/3 px-8 py-4 mt-20${
-                        obj.id % 2 === 0 ? " self-end" : ""
-                    }`}
-                >
-                    <div className="flex">
+        <>
+            <h2 className="text-4xl mt-6 text-slate-200 underline decoration-yellow-300 underline-offset-8 font-semibold">
+                Work
+            </h2>
+            <div className="flex flex-col">
+                {work.map((obj) => (
+                    <article
+                        key={obj.id}
+                        className={`box flex bg-red-500 md:w-2/3 px-8 py-4 mt-10 rounded${
+                            obj.id % 2 === 0 ? " md:self-end" : ""
+                        }`}
+                    >
                         <img
                             src={obj.img}
                             alt="Illustration du projet"
                             className="w-32"
                         />
-                        <p>{obj.text}</p>
-                    </div>
-                    <div>
-                        <a href={obj.demoLink}>Demo</a>
-                        <a href={obj.sourceCodeLink}>Code</a>
-                    </div>
-                </section>
-            ))}
-        </div>
+
+                        <div>
+                            <h3>{obj.title}</h3>
+                            <p>{obj.text}</p>
+
+                            <div>
+                                <a href={obj.demoLink}>Demo</a>
+                                <a href={obj.sourceCodeLink}>Code</a>
+                            </div>
+                        </div>
+                        <div className="top"></div>
+                        <div className="bottom"></div>
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </article>
+                ))}
+            </div>
+        </>
     );
 }
